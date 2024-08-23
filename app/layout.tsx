79c50847eadab374357eccdbc2dfd75387b3aaee
Victor Dutra <vsdutraa@gmail.com>
2024-08-23 19:32:17 -0300

    prisma, neon setup (admin)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Outfit } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from "@/providers/toast-provider";
 
 import "./globals.css";
 
@@ -23,6 +24,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={font.className}>
+          <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
