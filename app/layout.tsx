36686b5e2c29eb9d7ce93d211d63d049d6cdf505
Victor Dutra <vsdutraa@gmail.com>
2024-08-25 14:11:05 -0300

    navbar (admin)

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider afterSignOutUrl="/">
       <html lang="en">
         <body className={font.className}>
           <ToasterProvider />
